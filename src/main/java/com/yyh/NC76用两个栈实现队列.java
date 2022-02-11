a19a99bf3ca96c5f73f660014ee084a2d491bc55
YanHui-Yang <124481952@qq.com>
2022-02-11 15:33:22 +0800

    用两个栈实现队列.java

---
+++ src/main/java/com/yyh/NC76用两个栈实现队列.java
@@ -0,0 +1,21 @@
+package com.yyh;
+
+import java.util.Stack;
+
+public class NC76用两个栈实现队列 {
+
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.peek());
+            stack1.pop();
+        }
+        return stack2.pop();
+    }
+}
