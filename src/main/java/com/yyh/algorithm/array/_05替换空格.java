36f8d18afd015911ab0dac60f8f33e06010fb66a
YanHui-Yang <124481952@qq.com>
2022-07-17 16:16:57 +0800

    add: 05. 替换空格

---
+++ src/main/java/com/yyh/algorithm/array/_05替换空格.java
@@ -0,0 +1,34 @@
+package com.yyh.algorithm.array;
+
+/**
+ * 剑指 Offer 05. 替换空格
+ *
+ * https://leetcode.cn/problems/ti-huan-kong-ge-lcof/
+ *
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ *
+ * 示例 1：
+ *
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ *
+ */
+public class _05替换空格 {
+
+    public static void main(String[] args) {
+        String s = "We are happy.";
+        System.out.println(replaceSpace(s));
+    }
+
+    public static String replaceSpace(String s) {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ') {
+                builder.append("%20");
+            } else {
+                builder.append(s.charAt(i));
+            }
+        }
+        return builder.toString();
+    }
+}
